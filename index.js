e511ea3dc3468e1948ecedf2b0780c16ccf55295
Sergioo29 <sergiohds_29@hotmail.com>
2022-09-22 22:28:05 -0600

    DOM year las modif

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('date').textContent = new Date ().toLocaleDateString('en-US', options);
+const justYear = {year: 'numeric'};
+
+document.getElementById('date').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
+document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
