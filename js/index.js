3fb72e198084e180247bf2045b8fbf18d70e42f1
Sergioo29 <sergiohds_29@hotmail.com>
2022-10-13 23:43:17 -0600

    lil moa betta

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ const justYear = {year: 'numeric'};
 const todayDate = {day: 'short', day: 'numeric', month: 'short', year: 'numeric'};
 
 
-document.getElementById('date').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
-document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
 document.getElementById('today').textContent = new Date ().toLocaleDateString('en-US', todayDate);
+document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
+document.getElementById('date').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
