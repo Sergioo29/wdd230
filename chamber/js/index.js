767b14d1c6bb63f415865a8dca361cbf771e8d91
Sergioo29 <sergiohds_29@hotmail.com>
2022-10-14 10:33:46 -0600

    js go vrrrr

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,8 +1,11 @@
 const options = {day: 'numeric', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'};
 const justYear = {year: 'numeric'};
-const todayDate = {day:'numeric', month: 'long', year: 'numeric'};
 
+const datefield = document.querySelector(".today");
+const now = new Date();
 
 document.getElementById('date').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
 document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
-document.querySelector('.today').textContent = new Date ().toLocaleDateString('en-US', todayDate);
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `${fulldate}`;
