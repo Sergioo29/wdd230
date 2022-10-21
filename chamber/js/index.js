eb1dfea5403fa9193a41ba8a5b9856550dfc34f4
Sergioo29 <sergiohds_29@hotmail.com>
2022-10-21 11:17:48 -0600

    morestuff

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,17 +1,14 @@
 const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'};
 const justYear = {year: 'numeric'};
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(new Date());
 
-const datefield = document.querySelector(".today");
-const now = new Date();
 
 document.getElementById('date').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
 document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
-
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-datefield.innerHTML = `${fulldate}`;
+document.querySelector(".today").innerHTML = `${fulldate}`;
 
 
-function myFunction() {
+function hambButton() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -19,3 +16,8 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+/*  
+if () {
+  document.getElementById('todaysevent').textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+}; */
