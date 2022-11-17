8d2de0541d934b0ca9f8c2e2bbd710f381a9a3c5
Sergioo29 <sergiohds_29@hotmail.com>
2022-11-17 13:31:42 -0700

    newstuff

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,13 +1,50 @@
+let cardSelector = document.querySelector("#gridselect");
+let listSelector = document.querySelector("#listselect");
+let cardview = document.querySelector("#cardview");
+let listview = document.querySelector("#listview");
+
+cardSelector.addEventListener("click", (event) => {
+  cardview.style.display = "grid";
+  listview.style.display = "none";
+  cardSelector.style.opacity = "1.0";
+  listSelector.style.opacity = "0.5";
+}
+)
+listSelector.addEventListener("click", (event) => {
+  cardview.style.display = "none";
+  listview.style.display = "block";
+  cardSelector.style.opacity = "0.5";
+  listSelector.style.opacity = "1.0";
+}
+)
+
+
+
+
 function displayCard(card){
-    let cardview = document.querySelector("#cardview");
-    let cardIt = document.createElement("div");
-    cardIt.innerHTML = 
-      `<img src="${card.imageURL}">
-      <p>${card.name}</p>
-      <p>${card.street}</p>
-      <p>${card.phone}</p>
-      <p><a href="${card.website}">website</a></p>`;
     
+  let cardIt = document.createElement('section');
+
+  let img = document.createElement('img');
+  img.setAttribute('src', card.imageurl);
+
+  let h2 = document.createElement('h2');    
+  h2.textContent = `${card.name}`;
+
+  let p1 = document.createElement('p');
+  p1.textContent = `Address: ${card.street}`;
+
+  let p2= document.createElement('p');
+  p2.textContent = `Phone: ${card.phone}`;
+
+  
+
+  cardIt.appendChild(img);
+  cardIt.appendChild(h2);
+  cardIt.appendChild(p1);
+  cardIt.appendChild(p2);
+  
+  document.querySelector('div.cards').appendChild(cardIt);
 }
 
 function displayList(list){
