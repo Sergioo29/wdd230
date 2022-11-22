f87e149b7e91506c1180539c1c408aa08a4c32c1
Sergioo29 <sergiohds_29@hotmail.com>
2022-11-21 20:41:05 -0700

    miracle?

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,20 +1,36 @@
 
-function setwindchill(temp, windspeed){
+function showWeather(obj){
+    let tempobj = document.querySelector("#temperature");
+    let windspeedobj = document.querySelector("#windspeed");
+    let windchilldobj = document.querySelector("#windchill");
+    let weathericon = document.querySelector("#weathericon");
+    let weatherdesc = document.querySelector("#weatherdesc");
+    let iconURL = `https://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
 
-let windspeedobj = document.querySelector("#windspeed");
-let windchilldobj = document.querySelector("#windchill");
-let chill;
+    let windchillmsg = "N/A";
+    let temp = obj.main.temp;
+    let windspeed = obj.wind.speed;
 
-let windchillmsg = 'N/A';
+    if (temp <= 50 &&  windspeed > 3) {
+        chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16))) + " km/h";
+        windchillmsg = chill + " ° F";
+    };
 
-if (temp <= 50 &&  windspeed > 3) {
-    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16))) + " km/h";
-    windchillmsg = chill + " ° F";
-};
+    tempobj.textContent = temp;
+    windspeedobj.textContent = windspeed;
+    windchilldobj.innerHTML = windchillmsg;
+    weathericon.setAttribute ("src", iconURL);
+    weatherdesc = obj.weather[0].main;
+}
 
-windspeedobj.textContent = chill;
-windchilldobj.textContent = windchillmsg;
+const LAT = "64.8378";
+const LON = "-147.7164";
+const APIKEY = "87a2274e37afb2733385877c040c7a2d";
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
 
-};
 
-setwindchill(50, 10);
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        showWeather(jsObject);
+    });
