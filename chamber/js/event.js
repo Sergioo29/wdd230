4d21bc6eb4574e39f120b48d9aff0aed9b8a0edc
Sergioo29 <sergiohds_29@hotmail.com>
2022-11-29 16:15:08 -0700

    wdwd

---
+++ chamber/js/event.js
@@ -0,0 +1,3 @@
+if (todaysday.getDay() == 1 || todaysday.getDay() == 2) {
+    document.getElementById('todaysevent').textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  }; 
--- chamber/js/index.js
+++ chamber/js/index.js
@@ -17,8 +17,3 @@ function hambButton() {
       x.style.display = "flex";
     }
   }
-
-  
- if (todaysday.getDay() == 1 || todaysday.getDay() == 2) {
-   document.getElementById('todaysevent').textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
- }; 
