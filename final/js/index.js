26696ce4dfb4dec9db2527c751d4bbcd1d3c38a8
Sergioo29 <sergiohds_29@hotmail.com>
2022-12-14 09:41:58 -0700

    w

--- final/js/index.js
+++ final/js/index.js
@@ -1,3 +1,6 @@
-const currentYear = {year: 'numeric'};
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit'};
+const justYear = {year: 'numeric'};
 
-document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', currentYear);
+
+document.getElementById('modif').textContent = new Date (document.lastModified).toLocaleDateString('en-US', options);
+document.getElementById('year').textContent = new Date ().toLocaleDateString('en-US', justYear);
